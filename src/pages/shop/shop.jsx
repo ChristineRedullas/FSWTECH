75c22d3fee3ad8afee9a4df2055ef7277030d248
CRedCodes <100335168+CRedCodes@users.noreply.github.com>
2023-10-24 14:11:32 -0400

    Add to cart has been establish

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,5 +1,23 @@
-import React from "react"; 
+import React from "react";
+import { PRODUCTS } from "../../products";
+import { Product } from "./product";
+import "./shop.css";
 
 export const Shop = () => {
-    return <div>shop</div>;
-};
+  return (
+    <div className="shop">
+      <div className="shopTitle">
+        <h1>FSW TECH</h1>
+        <h3>One stop shop for all Bucs Tech</h3>
+      </div>
+
+      <div className="products">
+        {PRODUCTS.map((product) => (
+          <Product data={product} />
+        ))}
+      </div>
+    </div>
+  );
+}; 
+    /*import backgroundImage from "../../assets/products/Home.png";*/
+    /*<div className="shopback" style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }}>*/
---
+++ src/products.js
@@ -0,0 +1,53 @@
+import product1 from "./assets/products/iphone.png";
+import product2 from "./assets/products/macbuc.png";
+import product3 from "./assets/products/logibuc.png";
+import product4 from "./assets/products/airbucs.png";
+import product5 from "./assets/products/buctee.png";
+import product6 from "./assets/products/bucpic.png";
+
+
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "IPhoneBucs",
+        price: 999.00,
+        productImage: product1,
+    },
+
+    {
+        id: 2,
+        productName: "MacBuc",
+        price: 1999.00,
+        productImage: product2,
+    },
+
+    {
+        id: 3,
+        productName: "LogiBuc",
+        price: 39.00,
+        productImage: product3,
+    },
+
+    {
+        id: 4,
+        productName: "AirBucs",
+        price: 199.00,
+        productImage: product4,
+    },
+
+    {
+        id: 5,
+        productName: "BucTee",
+        price: 19.00,
+        productImage: product5,
+    },
+
+    {
+        id: 6,
+        productName: "PicBucs",
+        price: 2999.00,
+        productImage: product6,
+    },
+
+];
